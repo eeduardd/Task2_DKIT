10f85bdd25a508fb357f7d8de2f0738041290972
eeduardd <54077091+eeduardd@users.noreply.github.com>
2022-09-13 15:45:34 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_App_Task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _hasCode;
+        private bool _cancelChanges;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _cancelChanges = false;
+            var user = new User("Dkit", "dkit@test.com", "DkitGit");
+            _hasCode = user.GetHashCode();
+            DataContext = user;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (DataContext.GetHashCode() != _hasCode && !_cancelChanges)
+            {
+                MDSnackbarUnsavedChanges.IsActive = true;
+                e.Cancel = true;
+            }
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (TxbName.Text == "" | TxbGithub.Text == "" | TxbEmail.Text == "")
+            {
+                MDSnackbarUnfilledFields.IsActive = true;
+
+                
+            }
+            else if (DataContext.GetHashCode() != _hasCode && !_cancelChanges)
+            {
+                MDSnackbarUnsavedChanges.IsActive = true;
+            }
+        }
+
+        private void SnackbarMessage_ActionClick(object sender, RoutedEventArgs e)
+        {
+            MDSnackbarUnsavedChanges.IsActive = false;
+            _cancelChanges = true;
+            Close();
+        }
+
+        private void SnackbarMessage_UnfilledFields(object sender, RoutedEventArgs e)
+        {
+            MDSnackbarUnfilledFields.IsActive = false;
+            _cancelChanges = true;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_App_Task_1
+{
+    public class User : INotifyPropertyChanged
+    {
+        #region Private fields
+        private string _name;
+        private string _email;
+        private string _gitHub;
+        #endregion
+        #region Public property
+        public string Name
+        {
+            get { return _name; }
+            set { 
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { 
+                _email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+        public string GitHub
+        {
+            get { return _gitHub; }
+            set { 
+                _gitHub = value;
+                OnPropertyChanged("GitHub");
+            }
+        }
+        #endregion
+        #region PropertyChangedEventHandler
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+        public User(string name, string email, string gitHub)
+        {
+            _name = name;
+            _email = email;
+            _gitHub = gitHub;
+        }
+        public override int GetHashCode()
+        {
+            return (_name + _email + _gitHub).GetHashCode();
+        }
+    }
+}
